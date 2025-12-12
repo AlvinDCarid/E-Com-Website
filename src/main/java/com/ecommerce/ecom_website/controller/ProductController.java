b1fcb9fba99a5d1c9c6dd6752e8284bb62770d51
Alvin D. Carid <ab.sumro@gmail.com>
2025-12-12 18:31:27 +0500

    initial commit

---
+++ src/main/java/com/ecommerce/ecom_website/controller/ProductController.java
@@ -0,0 +1,90 @@
+package com.ecommerce.ecom_website.controller;
+
+import com.ecommerce.ecom_website.model.Product;
+import com.ecommerce.ecom_website.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProds() {
+        return new ResponseEntity<>(productService.getProds(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProdbyId(@PathVariable int id) {
+        Product product = productService.getProdbyId(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile) {
+        try {
+            Product pro = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(pro, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{id}/image")
+
+    public ResponseEntity<byte[]> getProductImage(@PathVariable int id) {
+        Product product = productService.getProdbyId(id);
+        byte[] imageFile = product.getImageData();
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
+    }
+
+    public ResponseEntity<String> updateProduct(@PathVariable int id,
+                                                @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile) {
+
+        Product pro = null;
+        try {
+            pro = productService.updateProduct(id, product, imageFile);
+
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+        if (pro != null) {
+            return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to update", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+        Product product = productService.getProdbyId(id);
+        if (product != null) {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/product/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+        List <Product> product = productService.searchProducts(keyword);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/ecommerce/ecom_website/model/Product.java
@@ -0,0 +1,33 @@
+package com.ecommerce.ecom_website.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
+}
